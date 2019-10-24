273e5a2e03e7af08c530beedd933657ce7f1365b
caramirezal <caramirezal@ciencias.unam.mx>
2019-10-24 17:04:38 +0200

    loading data from Chen S 2019

--- sc/SNAREseqDataAnalysis.R
+++ sc/SNAREseqDataAnalysis.R
@@ -3,9 +3,13 @@
 ## Dependencies
 library(Seurat)
 
+#############################################################################################
+
 ## Loading data into R
-dataDir <- '../data/chenS2019sameCellsData/GSE126074_AdBrainCortex_SNAREseq_cDNA/'
-adBrcDNA <- Read10X(data.dir = dataDir, 
+
+## Loading Adult brain cortex mRNA quantified transcripts
+cDNADir <- '../data/chenS2019sameCellsData/GSE126074_AdBrainCortex_SNAREseq_cDNA/'
+adBrcDNA <- Read10X(data.dir = cDNADir, 
                     gene.column = 1      ## necessary parameter, otherwise runs error
                     )
 adBrcDNA.Seu <- CreateSeuratObject(counts = adBrcDNA,
@@ -13,11 +17,25 @@ adBrcDNA.Seu <- CreateSeuratObject(counts = adBrcDNA,
                                    min.cells = 10,
                                    min.features = 500)
 
+## Loading Adult brain cortex chromatin accesibility reads
+chrDir <- '../data/chenS2019sameCellsData/GSE126074_AdBrainCortex_SNAREseq_chromatin/'
+adBrchr <- Read10X(data.dir = chrDir, 
+                    gene.column = 1      ## necessary parameter, otherwise runs error
+                    )
+adBrchr.Seu <- CreateSeuratObject(counts = adBrcDNA,
+                                   project = 'sameCellsData', 
+                                   min.cells = 10,
+                                   min.features = 500)
+
+
+###########################################################################################
+
+adBrcDNA.Seu[['percent.mt']] <- PercentageFeatureSet(adBrcDNA.Seu, pattern = '^MT-')
 VlnPlot(adBrcDNA.Seu, 
         features = c('nFeature_RNA', 
-                     'nCount_RNA',
-                     "percent.mt"),
+                     'nCount_RNA'),
         ncol = 1)
+
 
 
 
