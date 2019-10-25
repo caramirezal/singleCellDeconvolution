c664cc1c8717fc74b908783dd753ac7dfdcad07d
caramirezal <caramirezal@ciencias.unam.mx>
2019-10-25 17:11:19 +0200

    attempt to load files by name

--- sc/SNAREseqDataAnalysis.R
+++ sc/SNAREseqDataAnalysis.R
@@ -3,6 +3,7 @@
 ## Dependencies
 library(Seurat)
 
+
 #############################################################################################
 
 ## Loading data into R
@@ -17,6 +18,8 @@ adBrcDNA.Seu <- CreateSeuratObject(counts = adBrcDNA,
                                    min.cells = 10,
                                    min.features = 500)
 
+list.files(cDNADir)
+
 ## Loading Adult brain cortex chromatin accesibility reads
 chrDir <- '../data/chenS2019sameCellsData/GSE126074_AdBrainCortex_SNAREseq_chromatin/'
 adBrchr <- Read10X(data.dir = chrDir, 
@@ -27,6 +30,25 @@ adBrchr.Seu <- CreateSeuratObject(counts = adBrcDNA,
                                    min.cells = 10,
                                    min.features = 500)
 
+pattern <- 'GSE126074_AdBrainCortex_SNAREseq_cDNA' 
+data.dir <- '../data/chenS2019sameCellsData/'
+files <- grep(pattern, list.files(data.dir), value = TRUE)
+barcFileName <- grep('barcode', files, value = TRUE)
+featFileName <- grep('feature|gene|peak', files, value = TRUE)
+matrFilename <- grep('mtx', files, value = TRUE)
+filNames <- c(barcFileName, featFileName, matrFilename)
+filNames <- paste0(data.dir, filNames)
+names(filNames) <- c('barcodes', 'features', 'matrix')
+adBrcDNA <- Read10X(data.dir = filNames, 
+                    gene.column = 1      ## necessary parameter, otherwise runs error
+)
+
+Read10XbyPattern <- function(pattern, ...){
+        
+}
+
+p0BrcDNAdir <- '../data/chenS2019sameCellsData/'
+list.files(p0BrcDNAdir)
 
 ###########################################################################################
 
